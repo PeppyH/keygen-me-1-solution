a1b0024a3320c2b9e971c0f9c3ab10ba76613136
PeppyH <Jamieaustinprogramming@gmail.com>
2022-05-05 10:46:50 +1000

    fix: change const to let for variable that will change

--- keygen.js
+++ keygen.js
@@ -1,6 +1,6 @@
 const validChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-const key = [];
+let key = [];
 // generate the first 15 characters of the product key
 let keyOrdTotal = 0;
 for(let i = 0; i < 15; i++){
